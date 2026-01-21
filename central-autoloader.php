05cf886a91841db0d06364786f80591ccd9b595e
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-01-21 14:23:19 +0100

    initial setup

---
+++ central-autoloader.php
@@ -0,0 +1,12 @@
+<?php
+defined('ABSPATH') || exit;
+
+$loader = null;
+foreach ([ WP_CONTENT_DIR . '/vendor/autoload.php', ABSPATH . 'vendor/autoload.php' ] as $file) {
+    if (is_readable($file)) { $loader = require_once $file; break; }
+}
+
+if ($loader) {
+    define('PALASTHOTEL_COMPOSER_CENTRAL', true);
+    do_action('palasthotel/central_autoloader_loaded', $loader);
+}
