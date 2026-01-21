4a525c3b064798f976802571e773510562b79dd5
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-01-21 14:29:29 +0100

    chore: formatting

--- central-autoloader.php
+++ central-autoloader.php
@@ -2,8 +2,11 @@
 defined('ABSPATH') || exit;
 
 $loader = null;
-foreach ([ WP_CONTENT_DIR . '/vendor/autoload.php', ABSPATH . 'vendor/autoload.php' ] as $file) {
-    if (is_readable($file)) { $loader = require_once $file; break; }
+foreach ([WP_CONTENT_DIR . '/vendor/autoload.php', ABSPATH . 'vendor/autoload.php'] as $file) {
+    if (is_readable($file)) {
+        $loader = require_once $file;
+        break;
+    }
 }
 
 if ($loader) {
